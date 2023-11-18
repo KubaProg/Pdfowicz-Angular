21cead13a9b9312864cbfcb199c0fa2334602b18
Dominik Szczepanik <dominik16333@gmail.com>
2023-11-18 15:57:48 +0100

    adding photo and generating new pages implemented

--- Pdfowicz/src/app/components/dashboard.component.ts
+++ Pdfowicz/src/app/components/dashboard.component.ts
@@ -1,69 +1,40 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+// editor.component.ts
+import { Component, ViewChild, ElementRef } from '@angular/core';
+// @ts-ignore
+import html2pdf from 'html2pdf.js';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements AfterViewInit {
-  @ViewChild('canvas', {static: true}) canvas!: ElementRef<HTMLCanvasElement>;
-  pdfDataUri: string | null = null;
-  pageWidth = 595;
-  pageHeight = 842;
-  padding = 20;
-  fontSize = 20;
-  image: HTMLImageElement | null = null;
-  imageX = 50;
-  imageY = this.pageHeight - 50;
-  isDragging = false;
+export class DashboardComponent {
+  @ViewChild('pageTextarea') pageTextarea!: ElementRef<HTMLDivElement>;
 
-  ngAfterViewInit(): void {
-    this.createBlankPdf();
-  }
+  generatePDF(): void {
+    const content: HTMLElement | null = this.pageTextarea.nativeElement;
 
-  async createBlankPdf() {
-    const pdfDoc = await PDFDocument.create();
-    const page = pdfDoc.addPage([this.pageWidth, this.pageHeight]);
-    const canvas = this.canvas.nativeElement;
-    const ctx = canvas.getContext('2d');
-
-    if (ctx) {
-      ctx.clearRect(0, 0, this.pageWidth, this.pageHeight);
+    if (content) {
+      html2pdf(content);
     }
-
-    const div = document.querySelector('div[contenteditable="true"]');
-    const content = div ? div.innerHTML : '';
-
-    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-
-    const pageContent = this.transformHtmlTags(content);
-
-    page.drawText(pageContent, {
-      x: this.padding,
-      y: this.pageHeight - this.padding - this.fontSize,
-      size: this.fontSize,
-      font: font,
-      color: rgb(0, 0, 0),
-    });
-
-    const pdfBytes = await pdfDoc.save();
-    this.pdfDataUri = this.bytesToDataUri(pdfBytes);
   }
 
-  bytesToDataUri(pdfBytes: Uint8Array): string {
-    const binaryArray = Array.from(pdfBytes);
-    const binaryString = String.fromCharCode.apply(null, binaryArray);
-    return 'data:application/pdf;base64,' + btoa(binaryString);
+  handleImageDrop(event: DragEvent): void {
+    event.preventDefault();
+
+    const files = event.dataTransfer!.files;
+    if (files.length > 0) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const imageElement = document.createElement('img');
+        imageElement.src = e.target!.result as string;
+        this.pageTextarea.nativeElement.appendChild(imageElement);
+      };
+      reader.readAsDataURL(files[0]);
+    }
   }
 
-  updateCanvas(event: any): void {
-    this.createBlankPdf();
+  allowDrop(event: DragEvent): void {
+    event.preventDefault();
   }
-
-  transformHtmlTags(html: string): string {
-    const finalHtml = html.replace(/<\/div>/ig, "\n").replace(/<div>/ig, "").replace(/<br>/ig, "");
-    return finalHtml;
-  }
-
-
 }

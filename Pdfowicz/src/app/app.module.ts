911f29ad9ecf77e42fd1b496147648bd83410110
Jakub_Opielka <jakub.opielka@q-perior.com>
2023-11-22 21:00:49 +0100

    Kendo implemented

--- Pdfowicz/src/app/app.module.ts
+++ Pdfowicz/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {DashboardComponent} from "./components/dashboard.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { ButtonsModule } from "@progress/kendo-angular-buttons";
+import { IntlModule } from "@progress/kendo-angular-intl";
+import { PDFExportModule } from "@progress/kendo-angular-pdf-export";
+import {DashboardComponent} from "./components/dashboard.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    IntlModule,
+    PDFExportModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Pdfowicz/src/app/components/dashboard.component.ts
+++ Pdfowicz/src/app/components/dashboard.component.ts
@@ -1,140 +1,25 @@
-// editor.component.ts
-import {Component, ViewChild, ElementRef, OnInit, OnDestroy, AfterViewInit} from '@angular/core';
-// @ts-ignore
-import html2pdf from 'html2pdf.js';
-import { Path, Surface} from "@progress/kendo-drawing";
+
+import {Component, ViewChild,AfterViewInit} from '@angular/core';
+import {Surface} from "@progress/kendo-drawing";
 import {drawScene} from "../draw-scene";
+import {PDFExportComponent} from "@progress/kendo-angular-pdf-export";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements  OnDestroy, AfterViewInit{
+export class DashboardComponent implements  AfterViewInit{
 
-  @ViewChild('pageTextarea') pageTextarea!: ElementRef<HTMLDivElement>;
+  @ViewChild('pdf', { static: false }) pdf: PDFExportComponent;
   private surface!: Surface;
-  private drawingPath: Path | undefined;
-  isDrawing: boolean = false;
 
   ngAfterViewInit(): void {
-    this.surface = this.createSurface();
     drawScene(this.surface);
   }
 
-  ngOnDestroy(): void {
-    this.surface.destroy();
-    drawScene(this.surface)
-  }
-
-  generatePDF(): void {
-    const content: HTMLElement | null = this.pageTextarea.nativeElement;
-
-    if (content) {
-      html2pdf(content);
-    }
-  }
-
-  handleImageDrop(event: DragEvent): void {
-    event.preventDefault();
-
-    const files = event.dataTransfer!.files;
-    if (files.length > 0) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const imageElement = document.createElement('img');
-        imageElement.src = e.target!.result as string;
-        this.pageTextarea.nativeElement.appendChild(imageElement);
-      };
-      reader.readAsDataURL(files[0]);
-    }
-  }
-
-  allowDrop(event: DragEvent): void {
-    event.preventDefault();
-  }
-
-  startDrawing(event: MouseEvent): void {
-    if(this.isDrawing){
-      this.drawingPath = new Path({
-        stroke: { color: 'blue', width: 2 },
-      });
-
-      const offset = this.getOffset(event);
-      this.drawingPath.moveTo(offset.x, offset.y);
-      this.surface.draw(this.drawingPath);
-    }
-  }
-
-  handleMouseMove(event: MouseEvent): void {
-    if (this.drawingPath) {
-      const offset = this.getOffset(event);
-      this.drawingPath.lineTo(offset.x, offset.y);
-      this.surface.draw(this.drawingPath);
-    }
-  }
-
-  stopDrawing(): void {
-    this.drawingPath = undefined;
-  }
-
-  private createSurface(): Surface {
-    const nativeElement: HTMLDivElement = this.pageTextarea.nativeElement;
-    const surface = Surface.create(nativeElement);
-
-    nativeElement.addEventListener('mousedown', (event) => this.startDrawing(event));
-    nativeElement.addEventListener('mouseup', () => this.stopDrawing());
-    nativeElement.addEventListener('mousemove', (event) => this.handleMouseMove(event));
-
-    const svgElement = nativeElement.querySelector('svg');
-    if (svgElement) {
-      svgElement.style.height = '100%';
-      svgElement.style.width = '100%';
-      svgElement.style.position = 'absolute';
-      svgElement.style.left = '0';
-      svgElement.style.top = '0';
-    }
-
-    return surface;
-  }
-
-  private getOffset(event: MouseEvent): { x: number; y: number } {
-    // Check if the surface has a property that holds the HTML element
-    // @ts-ignore
-    const surfaceElement = this.surface['element'] || (this.surface as any).element;
-
-    if (!surfaceElement) {
-      console.error('Unable to find the surface element.');
-      return { x: 0, y: 0 };
-    }
-
-    const rect = surfaceElement.getBoundingClientRect();
-    return {
-      x: event.clientX - rect.left,
-      y: event.clientY - rect.top,
-    };
-  }
-
-  insertParagraph(): void {
-    const content: HTMLElement | null = this.pageTextarea.nativeElement;
-
-    if (content) {
-      const paragraphElement = document.createElement('p');
-      paragraphElement.textContent = 'New paragraph...';
-      content.appendChild(paragraphElement);
-    }
-  }
-
-  toggleDrawing(): void {
-    this.isDrawing = !this.isDrawing;
-
-    if (this.isDrawing) {
-      // Enable drawing logic
-      // For example, add event listeners or other drawing setup
-    } else {
-      // Disable drawing logic
-      // For example, remove event listeners or reset drawing state
-    }
+  exportAsPDF() {
+    this.pdf.saveAs('document.pdf');
   }
 
 }

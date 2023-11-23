ae5e4ba2db9ba6a174055dd87149d2d7435b3f83
Dominik Szczepanik <dominik16333@gmail.com>
2023-11-23 02:42:42 +0100

    adding via button and resizing photos

--- Pdfowicz/src/app/components/dashboard.component.ts
+++ Pdfowicz/src/app/components/dashboard.component.ts
@@ -1,18 +1,23 @@
-
-import {Component, ViewChild,AfterViewInit} from '@angular/core';
-import {Surface} from "@progress/kendo-drawing";
-import {drawScene} from "../draw-scene";
-import {PDFExportComponent} from "@progress/kendo-angular-pdf-export";
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { Surface } from "@progress/kendo-drawing";
+import { drawScene } from "../draw-scene";
+import { PDFExportComponent } from "@progress/kendo-angular-pdf-export";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements  AfterViewInit{
+export class DashboardComponent implements AfterViewInit {
 
   @ViewChild('pdf', { static: false }) pdf: PDFExportComponent;
+  @ViewChild('pageTextarea', { static: false }) pageTextarea: ElementRef;
+
   private surface!: Surface;
+  private currentRange: Range | null = null;
+  private resizingImage: HTMLImageElement | null = null;
+  private startX: number = 0;
+  private startY: number = 0;
 
   ngAfterViewInit(): void {
     drawScene(this.surface);
@@ -22,4 +27,82 @@ export class DashboardComponent implements  AfterViewInit{
     this.pdf.saveAs('document.pdf');
   }
 
+  addImage() {
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    fileInput.click();
+  }
+
+  handleFileInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const file = inputElement.files?.[0];
+
+    if (file) {
+      if (this.currentRange) {
+        this.insertImageFromFile(file);
+      } else {
+        this.appendImageToFile(file);
+      }
+    }
+
+    // Clear the file input to allow selecting the same file again
+    inputElement.value = '';
+  }
+
+  insertImageFromFile(file: File) {
+    const imageUrl = URL.createObjectURL(file);
+    const img = new Image();
+    img.src = imageUrl;
+    img.setAttribute('draggable', 'true');
+    img.addEventListener('mousedown', (e) => this.handleMouseDown(e));
+
+    if (this.currentRange) {
+      const fragment = this.currentRange.createContextualFragment(img.outerHTML);
+      this.currentRange.deleteContents();
+      this.currentRange.insertNode(fragment);
+    }
+  }
+
+  appendImageToFile(file: File) {
+    const imageUrl = URL.createObjectURL(file);
+    const img = new Image();
+    img.src = imageUrl;
+    img.setAttribute('draggable', 'true');
+    img.addEventListener('mousedown', (e) => this.handleMouseDown(e));
+
+    const pageTextarea = this.pageTextarea.nativeElement;
+    pageTextarea.appendChild(img);
+  }
+
+  saveSelection() {
+    const selection = window.getSelection();
+    this.currentRange = selection.getRangeAt(0);
+    // You can store the range or selection for later use if needed
+  }
+
+  handleMouseDown(event: MouseEvent) {
+    this.resizingImage = event.target as HTMLImageElement;
+    this.startX = event.clientX;
+    this.startY = event.clientY;
+    document.addEventListener('mousemove', (e) => this.handleMouseMove(e));
+    document.addEventListener('mouseup', () => this.handleMouseUp());
+  }
+
+  handleMouseMove(event: MouseEvent) {
+    if (this.resizingImage) {
+      const deltaX = event.clientX - this.startX;
+      const deltaY = event.clientY - this.startY;
+
+      this.resizingImage.width += deltaX;
+      this.resizingImage.height += deltaY;
+
+      this.startX = event.clientX;
+      this.startY = event.clientY;
+    }
+  }
+
+  handleMouseUp() {
+    this.resizingImage = null;
+    document.removeEventListener('mousemove', (e) => this.handleMouseMove(e));
+    document.removeEventListener('mouseup', () => this.handleMouseUp());
+  }
 }
